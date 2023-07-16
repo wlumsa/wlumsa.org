7e894bdf70c26597c5a5a78d6bd11a37124c32b2
Maham Farooq <mahamf100@gmail.com>
2023-07-15 23:20:22 -0400

    hero section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ed22bcf22b02d0b8"; }
+/******/ 		__webpack_require__.h = function() { return "08a6c3f78890bc9c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
