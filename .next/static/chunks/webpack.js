a629d9ef0091d483a254a5ef8c6791d2b9fdb8d1
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-07-18 23:37:10 -0400

    New Footer base design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7110a10aa6ec527f"; }
+/******/ 		__webpack_require__.h = function() { return "1fe1517b123c9fee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -7,8 +7,69 @@ import Image from "next/image";
 import Link from "next/link"
 const Footer:React.FC = () => {
   return (
-    
-    <footer className="footer p-10 bg-purple-950  w-full  gap-10 grid-flow-col">
+    <div className="w-full grid">
+        <footer className="footer p-10 bg-purple-950 text-base-content gap-0 ">
+            <div>
+                <span className="footer-title"> Quick Links</span>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+            </div>
+            <div>
+                <span className="footer-title"> University Guide</span>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+            </div>
+            <div>
+                <span className="footer-title"> Social </span>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+            </div>
+            <div>
+                <span className="footer-title"> Other </span>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+                <a className="link link-hover" href= "">Home</a>
+            </div>
+        </footer>
+        <footer className="footer px-10 py-4 border-t bg-purple-950 text-base-content border-[#B99393]">
+            <div className=" items-center grid-flow-col text-lg">
+                <Image src= {msalogo} alt = "logo"/>
+                <p>Wilfrid Laurier University <br/> Muslim Students Association</p>
+            </div>
+            <div className="md:place-self-center md:justify-self-end">
+                <div className="grid grid-flow-col gap-4">
+                    <a href="https://www.facebook.com/wlumsa/">
+                        <Image className="w-12 h-12  " src={fb} alt = "facebook"/>
+                    </a>
+                    
+                    <a href="https://www.instagram.com/wlumsa/">
+                        <Image className="w-12 h-12  " src={insta} alt = "fitnagram"/>
+                    </a>
+                    <a href="mailto:msa@wlu.ca">
+                        <Image className="w-12 h-12  " src={email} alt = "email"/>
+                    </a>
+                    <a href="">
+                        <Image className="w-20 h-12  " src={phone} alt = "phone #?"/>
+                </a>
+                </div>
+            </div>
+        </footer>
+    </div>
+  );
+};
+
+export default Footer;
+
+
+
+/*
+<footer className="footer p-10 bg-purple-950  w-full  gap-10 grid-flow-col">
         <div>
             <Image className="w-40 h-40  " src={msalogo} alt = "logo"/>
         </div>
@@ -32,9 +93,4 @@ const Footer:React.FC = () => {
         </div>
 
     </footer>
-    
-  );
-};
-
-export default Footer;
-
+    */
