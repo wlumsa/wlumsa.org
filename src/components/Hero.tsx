9fa7431292bf4958620933c6f722636eb50fcd09
Muhammad Saad <msaad3@proton.me>
2023-07-22 18:26:52 -0400

    Minor fixes & improvements

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,18 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-
-
-
-// import HeroBack from "../../images/background-image.jpeg"
-import arrowImage from '../../images/arrow2.png';
 
 const Hero:NextPage = () => {
   return (
     <div className="hero min-h-screen" style={{backgroundImage: "url(https://img.freepik.com/premium-photo/abstract-rainbow-colorful-bright-feather-closeup-up-macro-view-background-plumage-texture-with-dew-drops_753134-644.jpg)"}}>
-    {/* <div className="hero min-h-screen" style={{backgroun: HeroBack}}> */}
       <div className="hero-overlay bg-opacity-80"></div>
           <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
               <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
               <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
               <button className="btn btn-primary">  Join Us  </button>
-              <button className="arrow" style={{ width: 24, height: 24, backgroundImage: `url(${arrowImage})` }}></button>
+              <button className="arrow" style={{ width: 24, height: 24, backgroundImage: `url(https://img.icons8.com/ios-filled/50/expand-arrow--v1.png" alt="expand-arrow--v1)` }}></button>
             </div>
           </div>
-  </div>
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import Hero from "./hero";
+import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
