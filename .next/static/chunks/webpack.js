999c6a2c80bb700cafa030d6f0fac1f51cc84871
Muhammad Saad <msaad3@proton.me>
2023-07-22 17:34:47 -0400

    Add responsive Navbar
    - Has dummy text for now
    - Need to add branding

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1fe1517b123c9fee"; }
+/******/ 		__webpack_require__.h = function() { return "c42fdb1ee322d42f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
