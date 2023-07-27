3b1974b92292c862acdb9832f64831fc9d322a7b
Muhammad Saad <msaad3@proton.me>
2023-07-27 13:05:23 -0400

    Update hero

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8edfecff9f67753"; }
+/******/ 		__webpack_require__.h = function() { return "0619fe06683d09fc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,25 @@
 const Hero: React.FC = () => {
   return (
-    <div className="hero min-h-screen" style={{backgroundImage: "url(https://img.freepik.com/premium-photo/abstract-rainbow-colorful-bright-feather-closeup-up-macro-view-background-plumage-texture-with-dew-drops_753134-644.jpg)"}}>
-      <div className="hero-overlay bg-opacity-80"></div>
-          <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md">
-              <h1 className="mb-5 text-5xl font-bold">Welcome</h1>
-              <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-              <button className="btn btn-primary">  Join Us  </button>
-              <button className="arrow" style={{ width: 24, height: 24, backgroundImage: `url(https://img.icons8.com/ios-filled/50/expand-arrow--v1.png" alt="expand-arrow--v1)` }}></button>
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://dsai.ca/wp-content/uploads/WLU-Hero.jpg)'}
+                                           || {backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg))'}}>
+        <div className="hero-overlay bg-opacity-70" />
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-6xl font-bold">Salam</h1>
+            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <div className="join">
+              <button className="btn mx-1 text-primary bg-transparent border-primary hover:bg-primary hover:text-neutral-content hover:border-primary duration-200">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
+              </button>
+              <button className="btn mx-1 text-primary bg-transparent border-primary hover:bg-primary hover:text-neutral-content hover:border-primary duration-200">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
+              </button>
+              <button className="btn mx-1 text-primary bg-transparent border-primary hover:bg-primary hover:text-neutral-content hover:border-primary duration-200">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg>
+              </button>
             </div>
           </div>
+        </div>
     </div>
   );
 };
