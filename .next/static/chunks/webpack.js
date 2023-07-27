4199bbe5f79c4e989ec657b0ba2fc9e19e1cd960
Muhammad Saad <msaad3@proton.me>
2023-07-27 13:07:41 -0400

    Finish news w/o API

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "494a2a7de1780628"; }
+/******/ 		__webpack_require__.h = function() { return "3047131b6537f663"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,6 +1,6 @@
 const News: React.FC = () => {
     return (
-        <div className="flex flex-col px-6 pt-6 justify-center items-start">
+        <div className="flex flex-col px-6 py-10 justify-center items-start">
             <h3 className="text-2xl text-center pb-4 font-bold text-accent">Latest News</h3>
             <div className="carousel rounded-box gap-2">
                 <div id="key1" className="carousel-item">
