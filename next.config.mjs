feb9bf260465ab6944045fc0f6be2e6612745762
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-08-27 14:16:17 -0400

    nextconfig update

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  output:'export',
+
 
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
@@ -15,9 +15,11 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   images: {
-    unoptimized:true,
     remotePatterns: [
       {
         protocol: 'https',
