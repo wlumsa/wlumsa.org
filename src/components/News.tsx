eb1d72f60d3e92694757e14eff9d65b7f90f0e34
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-08-31 07:48:11 -0400

    news render mobile phone fix

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -2,6 +2,7 @@ import { InstagramEmbed } from 'react-social-media-embed';
 import React, { useEffect, useState } from "react";
 import { collection, getDocs, query, orderBy } from "firebase/firestore"; // Import orderBy
 import db from "../firebase";
+import { Suspense } from 'react'
 
 interface InstagramPost {
     link:string;
@@ -29,14 +30,12 @@ const News: React.FC = () => {
     return (
         <div id="news" className="flex flex-col px-0 py-10 justify-center items-start max-w-6xl bg-base-100">
             <h3 className="text-3xl text-center py-3 pb-4 font-bold text-primary">Latest News</h3>
-            
-            <div className="carousel rounded-box gap-2 shadow  py-3  w-[325px] xl:w-[1180px] lg:w-[924px]   md:w-[668px] sm:w-[540px] ">
-                 {instagramPosts.map((post, index) => (
-                    <div id = {`item${(index+1).toString()}`}className="carousel-item  rounded-xl ">
-                        <InstagramEmbed url = {post.link} height={425}    />
-                    </div>
-                ))}
-                
+            <div className="carousel rounded-box py-3 bg-primary w-screen  lg:max-w-6xl">
+                    {instagramPosts.map((post,index)=>(
+                        <div id = {`item${(index+1).toString()}`} className="carousel-item relative px-2 ">
+                            <InstagramEmbed url = {post.link} height={425}/>
+                        </div>
+                    ))}
             </div>
             <div className='flex justify-center w-full py-2 gap-2'>
                 {Array.from({ length: queryLength }, (_, index) => (
