8a313490765a19be4911ff2b31150c6850a967e3
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-11-06 05:49:40 -0500

    updated vercel analytics + Email

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,12 @@ import { Analytics } from '@vercel/analytics/react';
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-  <Analytics />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default MyApp;
