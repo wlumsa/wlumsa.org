0dc56cb01d11d4a6ce71bfefe97b0cc3ffcd9c30
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-11-30 20:31:56 -0500

    changed logic of prayertimings page

--- src/pages/prayertimings.tsx
+++ src/pages/prayertimings.tsx
@@ -4,74 +4,73 @@ import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import db from "../firebase";
 import Footer from "~/components/Footer";
 interface Timings {
-    Fajr: string;
-    Sunrise: string;
-    Dhuhr: string;
-    Asr: string;
-    Maghrib: string;
-    Isha: string;
+  Fajr: string;
+  Sunrise: string;
+  Dhuhr: string;
+  Asr: string;
+  Maghrib: string;
+  Isha: string;
+}
+
+interface TimingsData {
+  timings: Timings;
+  date: string; // This will be a formatted date string
+}
+
+interface JummahItem {
+  time: string;
+  room: string;
+}
+
+const fetchTimings = async (): Promise<TimingsData[]> => {
+  const adhanCollectionRef = collection(db, "AdhanTimings");
+  const q = query(adhanCollectionRef, orderBy("date", "asc"));
+  const querySnapshot = await getDocs(q);
   
-  }
-  
-  interface TimingsData {
-    timings: Timings;
-    date: string;
-  }
-  
-  interface JummahItem {
-    time: string;
-    room: string;
-  }
-  
-  const fetchTimings = async (): Promise<TimingsData[]> => {
-    const adhanCollectionRef = collection(db, "AdhanTimings");
-    const q = query(adhanCollectionRef, orderBy("date", "asc")); // Order by date
-    const querySnapshot = await getDocs(q);
-    
-    return querySnapshot.docs.map(doc => ({
+  return querySnapshot.docs.map(doc => {
+    // Format the Firebase timestamp to a date string
+    const timestamp = doc.data().date;
+    const date = timestamp.toDate().toDateString();
+    return {
       timings: doc.data() as Timings,
-      date: doc.id  // Assuming the date is the document ID
-    }));
-  };
-  
+      date // Now this is a human-readable date string
+    };
+  });
+};
 
 
-  const PrayerTimes: React.FC = () => {
-    const [timingsData, setTimingsData] = useState<TimingsData[]>([]);
-    const [jummahInfo, setJummahInfo] = useState<JummahItem[]>([]);
-    const [iqamahTimes, setIqamahTimes] = useState<Timings | null>(null);
+const PrayerTimes: React.FC = () => {
+  const [timingsData, setTimingsData] = useState<TimingsData[]>([]);
+  const [jummahInfo, setJummahInfo] = useState<JummahItem[]>([]);
+  const [iqamahTimes, setIqamahTimes] = useState<Timings | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const nextWeekData = await fetchTimings();
+        setTimingsData(nextWeekData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
   
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const nextWeekData = await fetchTimings();
-          setTimingsData(nextWeekData);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-    
-     
+    const fetchJummahInfo = async () => {
+      const jummahCollectionRef = collection(db, "Jummah");
+      const querySnapshot = await getDocs(jummahCollectionRef);
+      setJummahInfo(querySnapshot.docs.map(doc => doc.data() as JummahItem));
+    };
   
-      const fetchJummahInfo = async () => {
-        const jummahCollectionRef = collection(db, "Jummah");
-        const querySnapshot = await getDocs(jummahCollectionRef);
-        setJummahInfo(querySnapshot.docs.map(doc => doc.data() as JummahItem));
-      };
+    const fetchIqamahTimes = async () => {
+      const iqamahTimesRef = collection(db, "IqamahTimings");
+      const snapshot = await getDocs(iqamahTimesRef);
+      const iqamahTimesData = snapshot.docs[0]?.data() as Timings | undefined;
+      setIqamahTimes(iqamahTimesData || null);
+    };
   
-      const fetchIqamahTimes = async () => {
-        const iqamahTimesRef = collection(db, "IqamahTimings");
-        const snapshot = await getDocs(iqamahTimesRef);
-      
-        const iqamahTimesData = snapshot.docs[0]?.data() as Timings | undefined;
-        console.log(iqamahTimesData)
-        setIqamahTimes(iqamahTimesData || null);
-      };
-  
-      fetchData();
-      fetchJummahInfo();
-      fetchIqamahTimes();
-    }, []);
+    fetchData();
+    fetchJummahInfo();
+    fetchIqamahTimes();
+  }, []);
 
     const convertTo12HourFormat = (timeString?: string): string => {
       if (!timeString) {
@@ -106,23 +105,18 @@ interface Timings {
   };
   
 
-  const prayerNames: (keyof Timings)[] = [
-    'Fajr',
-    'Sunrise',
-    'Dhuhr',
-    'Asr',
-    'Maghrib',
-    'Isha',
-  ];
+ 
 
+  const getDayOfWeek = (dateString: string): string => {
+    return new Date(dateString).toLocaleDateString(undefined, { weekday: 'long' });
+  };
+
+  const prayerNames: (keyof Timings)[] = [
+    'Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha',
+  ];
   const getAllJummahTimesAndRooms = (): string[] => {
     return jummahInfo.map(jummah => `${jummah.time} in ${jummah.room}`);
   };
-  const formatDate = (dateString: string): string => {
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-  };
-
   const transposedData = prayerNames.map((prayerName) => ({
     prayerName,
     timings: timingsData.map((dayTimings) => {
@@ -148,42 +142,42 @@ interface Timings {
   
   return (
     <div className="pt-16 flex flex-col items-center">
-      <Navbar />
-      <div className="container mx-auto p-8">
-        <h1 className="text-3xl font-bold mb-4">Prayer Times</h1>
-        <p className="text-gray-600 mb-4">
-          <span className="text-green-600 font-bold">Green</span> = Iqamah times<br />
-          <span className="text-black font-bold">Black</span> = Adhan Times  
-        </p>
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white text-black rounded-lg shadow-lg">
-            <thead className="bg-primary text-white">
-              <tr>
-                <th className="px-4 py-2 font-bold text-left text-xs uppercase border border-black">
-                  Prayer Time
-                </th>
-                {timingsData.map((dayTimings) => (
-                  <th
+    <Navbar />
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-4">Prayer Times</h1>
+      <p className="text-gray-600 mb-4">
+        <span className="text-green-600 font-bold">Green</span> = Iqamah times<br />
+        <span className="text-black font-bold">Black</span> = Adhan Times  
+      </p>
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white text-black rounded-lg shadow-lg">
+          <thead className="bg-primary text-white">
+            <tr>
+              <th className="px-4 py-2 font-bold text-left text-xs uppercase border border-black">
+                Prayer Time
+              </th>
+              {timingsData.map((dayTimings) => (
+                <th
                   key={dayTimings.date}
                   className="px-4 py-2 font-bold text-left text-xs uppercase border border-black"
                 >
-                  {formatDate(dayTimings.date)}
+                  {getDayOfWeek(dayTimings.date)}
                 </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody className="bg-base-200">
-              {transposedData.map((prayerData) => (
-                <tr key={prayerData.prayerName}>
-                  <td className="px-4 py-2 font-bold text-left text-xs uppercase border border-black">
-                    {prayerData.prayerName}
-                  </td>
-                  {prayerData.timings.map((timing) => (
-                    <td key={timing.date} className={`px-4 py-2 whitespace-nowrap border ${
-                      timing === prayerData.timings[0] ? "border-l border-black" : "border-black"
-                    } ${prayerData === transposedData[0] ? "border-t" : ""}`}>
-                
-                      <div className="text-black">{convertTo12HourFormat(timing.time)}</div>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="bg-base-200">
+            {transposedData.map((prayerData) => (
+              <tr key={prayerData.prayerName}>
+                <td className="px-4 py-2 font-bold text-left text-xs uppercase border border-black">
+                  {prayerData.prayerName}
+                </td>
+                {prayerData.timings.map((timing) => (
+                  <td key={timing.date} className={`px-4 py-2 whitespace-nowrap border ${
+                    timing === prayerData.timings[0] ? "border-l border-black" : "border-black"
+                  } ${prayerData === transposedData[0] ? "border-t" : ""}`}>
+              
+                    <div className="text-black">{convertTo12HourFormat(timing.time)}</div>
                       {/* Check for Dhuhr on Friday for Jummah times */}
                       {prayerData.prayerName === 'Dhuhr' && new Date(timing.date).getDay() === 5
                         ? <div className="text-green-600">
@@ -207,7 +201,7 @@ interface Timings {
           </table>
         </div>
         <p>
-          Based on ISNA with Hanafi Asr calculation method. 
+          Based on Waterloo Masjid timings and Hanafi Asr calculation
         </p>
       </div>
       <Footer/>
