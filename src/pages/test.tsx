cba4b29d6245f5b202cb094500fe236f9a21c0c9
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-12-30 19:55:19 -0500

    added types.tsx

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 const test = () => {
   return (
-    <div><iframe
-    src="https://docs.google.com/forms/d/e/1FAIpQLSfRHkZmrcxuMcZkmxAi_X2O9nOK24DA7pu_ZuwsxNeWjoLyMw/viewform?embedded=true"
-    width="640"
-    height="593"
-    // @ts-ignore
-    frameborder="0"
-    marginheight="0"
-    marginwidth="0"
-  >
-    Loading…
-  </iframe>
+    <div>
+      <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfRHkZmrcxuMcZkmxAi_X2O9nOK24DA7pu_ZuwsxNeWjoLyMw/viewform?embedded=true"
+      width="640"
+      height="593"
+      // @ts-ignore
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+    >
+      Loading…
+    </iframe>
   </div>
   )
 }
