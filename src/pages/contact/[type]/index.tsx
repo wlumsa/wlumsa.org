53fe55be0c8d58aa381c35723fdc120e9d4164b9
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-01-07 13:37:12 -0500

    added dynamic forms with staticpaths

--- src/pages/contact/[type]/index.tsx
+++ src/pages/contact/[type]/index.tsx
@@ -1,9 +1,61 @@
 import React from "react";
 import { useRouter } from "next/router";
 import CtaForm from "~/components/Forms/CtaForm";
+import { GetStaticPaths } from "next";
+import { NextPage } from "next";
+import { GetStaticProps } from "next";
+import { getNavbarData, getFooterData, fetchSocialLinks } from "~/lib/api";
+import Navbar from "~/components/Global/Navbar";
+import Footer from "~/components/Global/Footer";
+export const getStaticPaths: GetStaticPaths = async () => {
+  
+  const types = ['Incident', 'Support', 'Fiqh'];
+
+  
+  const paths = types.map((type) => ({
+    params: { type },
+  }));
 
 
-const Index = () => {
+  return { paths, fallback: false };
+};
+
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const socialLinks = await fetchSocialLinks();
+    const navbarData = await getNavbarData();
+    const footerData = await getFooterData();
+    return {
+      props: {
+        socialLinks,
+        navbarData,
+        footerData,
+      },
+      revalidate: 43200, // or however many seconds you prefer
+    };
+  } catch (error) {
+    console.error("Error in getStaticProps:", error);
+    return {
+      props: {
+        socialLinks: [],
+        navbarData: [],
+        footerdata: [],
+      },
+    };
+  }
+};
+interface DynamicContactPageProps {
+  socialLinks: SocialLinkProps[];
+  navbarData: NavbarGroup[];
+  footerData: FooterGroup[];
+}
+const DynamicContact: NextPage<DynamicContactPageProps> = ({
+  socialLinks,
+  navbarData, // Add this line
+  footerData,
+}) => {
   const router = useRouter();
   const category = Array.isArray(router.query.type)
     ? router.query.type[0]
@@ -15,8 +67,9 @@ const Index = () => {
   }
 
   return (
-    <div>
-      <div className="mt-20 flex flex-col items-center">
+    <div className="flex min-h-screen flex-col">
+      <Navbar navbarData={navbarData} />
+      <div className="mt-20 flex flex-col items-center flex-grow">
         <div className="mb-10">
           <h3 className="pt-4 text-center text-3xl font-bold text-primary duration-200 hover:scale-105 lg:pt-0">
             {category} Form!
@@ -27,8 +80,9 @@ const Index = () => {
         </div>
         <CtaForm category={category} />
       </div>
+      <Footer footerGroups={footerData} socialLinks={socialLinks} />
     </div>
   );
 };
 
-export default Index;
+export default DynamicContact;
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,11 +1,48 @@
 import React from "react";
 import CtaForm from "~/components/Forms/CtaForm";
-
-
-const Contact = () => {
+import { GetStaticProps } from "next";
+import Navbar from "~/components/Global/Navbar";
+import Footer from "~/components/Global/Footer";
+import { getNavbarData, getFooterData, fetchSocialLinks } from "~/lib/api";
+import { NextPage } from "next";
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const socialLinks = await fetchSocialLinks();
+    const navbarData = await getNavbarData();
+    const footerData = await getFooterData();
+    return {
+      props: {
+        socialLinks,
+        navbarData,
+        footerData,
+      },
+      revalidate: 43200, // or however many seconds you prefer
+    };
+  } catch (error) {
+    console.error("Error in getStaticProps:", error);
+    return {
+      props: {
+        socialLinks: [],
+        navbarData: [],
+        footerdata: [],
+      },
+    };
+  }
+};
+interface ContactPageProps {
+  socialLinks: SocialLinkProps[];
+  navbarData: NavbarGroup[];
+  footerData: FooterGroup[];
+}
+const Contact: NextPage<ContactPageProps> = ({
+  socialLinks,
+  navbarData, // Add this line
+  footerData,
+}) => {
   return (
-    <div>
-      <div className="mt-20 flex flex-col items-center">
+    <div className="flex min-h-screen flex-col">
+      <Navbar navbarData={navbarData} />
+      <div className="mt-20 flex flex-col items-center flex-grow">
         <section className="bg-base-100" id="contact">
           <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
             <div className="mb-4">
@@ -66,6 +103,7 @@ const Contact = () => {
           </div>
         </section>
       </div>
+      <Footer footerGroups={footerData} socialLinks={socialLinks} />
     </div>
   );
 };
