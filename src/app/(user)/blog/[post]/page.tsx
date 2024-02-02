f698adcb10df1496ceccaba371e053a8146c42f6
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-02-01 19:28:22 -0500

    fetching id in blog post page

--- src/app/(user)/blog/[post]/page.tsx
+++ src/app/(user)/blog/[post]/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
 
-const page = () => {
+import React from "react";
+import { useParams } from "next/navigation";
+
+
+import { fetchProduct } from "@/Utils/api";
+
+
+import ProductDisplay from "@/components/UI/ProductComponent";
+
+export default async function ProductPage({
+  params,
+}: {
+  params: { post: string };
+}) {
+  const id = params.post;
+
+  if (!id) {
+    return <div>Post not found</div>;
+  }
   return (
-    <div>page</div>
-  )
+    <div className="flex min-h-screen flex-col py-10">
+      <h1 className="mt-20">{id}</h1>
+    </div>
+  );
 }
 
-export default page
+
