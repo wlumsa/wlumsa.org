6c0863493cfdd29cd93a2c48e33dd22ce7a994e8
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-02-17 23:30:09 -0500

    added next auth google sign in

--- auth.tsx
+++ auth.tsx
@@ -1,11 +1,15 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
 
+import Google from "next-auth/providers/google";
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
-  providers: [GitHub],
+  providers: [Google({
+    clientId:process.env.GOOGLE_CLIENT_ID,
+    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+  })],
+  
 });
---
+++ src/app/(user)/auth/page.tsx
@@ -0,0 +1,45 @@
+import { auth, signIn, signOut } from "auth";
+
+function SignIn() {
+  return (
+    <div className="mt 10">
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <p>You are not logged in</p>
+      <button type="submit">Sign in with google</button>
+    </form>
+    </div>
+  );
+}
+
+function SignOut({ children }: { children: React.ReactNode }) {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <p>{children}</p>
+      <button type="submit">Sign out</button>
+    </form>
+  );
+}
+
+export default async function Page() {
+  let session = await auth();
+  let user = session?.user?.email;
+  console.log(session)
+
+
+  return (
+    <section className="mt-20">
+      <h1>Home</h1>
+      <div>{user ? <SignOut>{`Welcome ${user} ${session?.user?.image}`}</SignOut> : <SignIn />}</div>
+    </section>
+  );
+}
