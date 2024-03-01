804681bb27a0ff4a3e0f513e4b6bf2685748a68f
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-03-01 00:04:05 -0500

    larger font size

--- src/app/(user)/photouploader/page.tsx
+++ src/app/(user)/photouploader/page.tsx
@@ -9,11 +9,10 @@ export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="flex flex-col  flex-grow items-center justify-center">
-        <h1 className="text-center text-md lg:text-4xl font-bold text-primary mb-20 ">
+        <h1 className="text-center text-lg lg:text-4xl font-bold text-primary mb-20 ">
           Upload your photos and videos here!
         </h1>
         <div className="w-full flex flex-col items-center justify-center px-4">
-
           <UploadPhotoForm />
         </div>
       </div>
