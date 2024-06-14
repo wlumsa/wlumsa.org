0116458d6f1c2ff3a63d7e2e37dfe13c74d77d84
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-06-14 12:25:41 -0400

    added server only in order to keep client-server architecture seperate

--- src/Utils/supabase/datafetcher.ts
+++ src/Utils/supabase/datafetcher.ts
@@ -1,4 +1,4 @@
-
+import 'server-only'
 import { createClient } from "./server";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL 
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
