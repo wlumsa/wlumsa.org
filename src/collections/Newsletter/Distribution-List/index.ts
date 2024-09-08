328f81e5cd229238b22785a5607d6ca2023cddb2
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-09-08 08:17:03 -0400

    added dynamic api route

--- src/collections/Newsletter/Distribution-List/index.ts
+++ src/collections/Newsletter/Distribution-List/index.ts
@@ -13,7 +13,7 @@ export const DistributionList: CollectionConfig = {
         label:"List Name",
         type:"text",
         required:true,
-    },
+    }, 
     {
       name: "emails",
       label: "emails",
