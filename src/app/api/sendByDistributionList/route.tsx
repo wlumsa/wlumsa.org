749d7c8f4524dfa5bcca627c8c529f992832bf7f
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-09-08 20:27:07 -0400

    api route now returns next response object

--- src/app/api/sendByDistributionList/route.tsx
+++ src/app/api/sendByDistributionList/route.tsx
@@ -3,10 +3,7 @@ import Newsletter from 'emails/general';
 import { Individual } from '@/payload-types';
 import { resend } from '@/Utils/resend';
 import { getPublicURL } from '@/Utils/datafetcher';
-
-//const resend = new Resend("re4Chuui1_5MzcEdVbdi5d626LGU3gWVyr" );
 import { convertRichTextToMarkdown } from '@/Utils/converter';
-
 
 function chunkArray(array: Individual[], chunkSize: number) {
   const chunks = [];
@@ -16,41 +13,55 @@ function chunkArray(array: Individual[], chunkSize: number) {
   return chunks;
 }
 
-export const POST = async (req: Request) => {
+export async function POST(req: Request) {
   try {
     const response = await req.json();
     const { title, subject, headerImage, publishedAt, content, distributionListId, content_html } = response;
-    console.log(content_html)
+    console.log(content_html);
     const distributionList: (number | Individual)[] | null | undefined = await getDistributionList(distributionListId);
-    //checks to make sure distributionList is not a number
+    
     if (!distributionList || !Array.isArray(distributionList)) {
       throw new Error('Invalid distribution list');
     }
+    
     const validDistributionList: Individual[] = distributionList.filter((item): item is Individual => {
       return typeof item === 'object' && 'email' in item;
     });
-    console.log(validDistributionList)
+    
+    console.log(validDistributionList);
     const chunks = chunkArray(validDistributionList, 100);
-    chunks.forEach(async (chunk) => {
-
+    
+    const results = await Promise.all(chunks.map(async (chunk) => {
       const batch = chunk.map((user) => ({
         from: 'admin@wlumsa.org',
         to: [user.email],
         subject: subject,
         react: Newsletter({ firstName: user.firstName, content: content_html }),
       }));
-      console.log(batch)
-      const { data, error } = await resend.batch.send(batch)
+      
+      console.log(batch);
+      const { data, error } = await resend.batch.send(batch);
+      
       if (error) {
-        return Response.json({ error }, { status: 500 });
+        console.log(error);
+        return { error };
       }
-  
-      return Response.json(data);
-    });
+      
+      console.log(data);
+      return { data };
+    }));
+
+    const errors = results.filter(result => result.error);
+    if (errors.length > 0) {
+      return Response.json({ errors }, { status: 500 });
+    }
+
+    return Response.json({ message: "Emails sent successfully" }, { status: 200 });
+
   } catch (error) {
     console.error("Error sending email:", error);
     return new Response(`Internal server error ${error}`, {
       status: 500,
     });
   }
-};
+}
