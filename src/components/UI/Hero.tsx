1bb78f6162e0d7bbfc0cedaed3f96143e3a21204
rzlm <razan4424@gmail.com>
2024-09-25 08:04:38 -0400

    buttons

--- src/components/UI/Hero.tsx
+++ src/components/UI/Hero.tsx
@@ -26,6 +26,10 @@ const Hero: React.FC<HeroProps> = ({ mediaDocs, socialLinks }) => {
           <FadeText className="mb-5 text-6xl font-bold text-secondary duration-200 hover:scale-105" direction="right" text="Salam!" />
 
           <TextEffect   className="mb-5 text-white" per="char" as="h2">"The believers are but brothers, so make settlement between your brothers. And fear Allāh that you may receive mercy." (Quran 49:10)</TextEffect>
+          <div className="flex flex-row m-4 justify-center mb-6" >
+            <button className="btn btn-primary text-white  mx-4"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfwn-5xuz58a9nzINqZoofyiMr-C7lphMs5KesnzVOB1jrXNg/viewform">Donate</a></button>
+            <button className=" btn btn-secondary mx-4 "><a href="/guidebook">Become a Member</a></button>
+          </div>
           <div className="flex flex-row items-center justify-center gap-4">
             {socialLinks.map((social, index) => (
               <a
@@ -45,6 +49,7 @@ const Hero: React.FC<HeroProps> = ({ mediaDocs, socialLinks }) => {
               </a>
             ))}
           </div>
+         
         </div>
       </div>
     </div>
