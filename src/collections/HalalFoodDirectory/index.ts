ae29742e280c94366dc9c0565306ed17619d1d90
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-10-03 18:39:41 -0400

    updated group

--- src/collections/HalalFoodDirectory/index.ts
+++ src/collections/HalalFoodDirectory/index.ts
@@ -3,79 +3,80 @@
 import { CollectionConfig } from "payload";
 
 export const HalalDirectory: CollectionConfig = {
-  slug: 'halal-directory',
+  slug: "halal-directory",
   admin: {
-    useAsTitle: 'name',
+    useAsTitle: "name",
+
+    group: "Admin",
   },
   fields: [
     {
-      name: 'name',
-      type: 'text',
+      name: "name",
+      type: "text",
       required: true,
-      label: 'Name', // Matches 'name' from spreadsheet
+      label: "Name", // Matches 'name' from spreadsheet
     },
     {
-      name: 'category',
-      type: 'select',
+      name: "category",
+      type: "select",
       options: [
-        { label: 'Chinese', value: 'chinese' },
-        { label: 'Persian', value: 'persian' },
-        { label: 'Shawarma', value: 'shawarma' },
-        { label: 'Burgers', value: 'burgers' },
-        { label: 'Bangladeshi', value: 'bangladeshi' },
-        { label: 'Chinese Indo Fusion', value: 'chinese-indo-fusion' },
-        { label: 'Pakistani Food', value: 'pakistani-food' },
-        { label: 'Chicken and Waffles', value: 'chicken-and-waffles' },
-        { label: 'Kabob', value: 'kabob' },
-        { label: 'Uyghur', value: 'uyghur' },
-        { label: 'Chicken', value: 'chicken' },
-        { label: 'Indian Fusion Food', value: 'indian-fusion-food' },
-        { label: 'Pizza', value: 'pizza' }
+        { label: "Chinese", value: "chinese" },
+        { label: "Persian", value: "persian" },
+        { label: "Shawarma", value: "shawarma" },
+        { label: "Burgers", value: "burgers" },
+        { label: "Bangladeshi", value: "bangladeshi" },
+        { label: "Chinese Indo Fusion", value: "chinese-indo-fusion" },
+        { label: "Pakistani Food", value: "pakistani-food" },
+        { label: "Chicken and Waffles", value: "chicken-and-waffles" },
+        { label: "Kabob", value: "kabob" },
+        { label: "Uyghur", value: "uyghur" },
+        { label: "Chicken", value: "chicken" },
+        { label: "Indian Fusion Food", value: "indian-fusion-food" },
+        { label: "Pizza", value: "pizza" },
       ],
       required: true,
-      label: 'Category',
+      label: "Category",
     },
-    
+
     {
-      name: 'slaughtered',
-      type: 'select',
+      name: "slaughtered",
+      type: "select",
       options: [
-        { label: 'Hand', value: 'hand' },
-        { label: 'Machine', value: 'machine' },
-        {label: 'Both', value: 'both'},
-        {label: 'N/A', value: 'n/a'}
+        { label: "Hand", value: "hand" },
+        { label: "Machine", value: "machine" },
+        { label: "Both", value: "both" },
+        { label: "N/A", value: "n/a" },
       ],
-      label: 'Slaughter',
+      label: "Slaughter",
     },
     {
-      name: 'shortDescription',
-      type: 'textarea',
+      name: "shortDescription",
+      type: "textarea",
       required: true,
-      label: 'Description', // Matches 'description' from spreadsheet
+      label: "Description", // Matches 'description' from spreadsheet
     },
     {
-      name: 'location',
-      type: 'text',
+      name: "location",
+      type: "text",
       required: true,
-      label: 'Location', // Matches 'location' from spreadsheet
+      label: "Location", // Matches 'location' from spreadsheet
     },
     {
-      name: 'googleMapsLink',
-      type: 'text',
+      name: "googleMapsLink",
+      type: "text",
       required: true,
-      label: 'Map', // Matches 'map' from spreadsheet
+      label: "Map", // Matches 'map' from spreadsheet
     },
     {
-      name: 'website',
-      type: 'text',
-      label: 'Website', // Matches 'website' from spreadsheet
+      name: "website",
+      type: "text",
+      label: "Website", // Matches 'website' from spreadsheet
     },
     {
-      name:"image",
-      type:"relationship",
-      label:"Image",
-      relationTo:"media",
-    }
+      name: "image",
+      type: "relationship",
+      label: "Image",
+      relationTo: "media",
+    },
   ],
 };
-
