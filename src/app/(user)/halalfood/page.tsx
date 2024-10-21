c8a9572772522efce990fc4a10e05cfe2fcf40a5
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-10-20 21:23:53 -0400

    filter for on/off campus fixed on halalfood directory

--- src/app/(user)/halalfood/page.tsx
+++ src/app/(user)/halalfood/page.tsx
@@ -15,6 +15,7 @@ interface HalalDirectoryData {
   website?: string;
   image_id?: string;
   campusLocation: string;
+  is_on_campus: boolean;
 }
 
 // Options for cuisine types used in the dropdown filter
@@ -91,10 +92,10 @@ export default function HalalDirectoryPage() {
   const filterData = () => {
     return halalDirectory
       .filter(item => {
-        const matchesCategory = selectedCuisine === 'All Cuisines' || item.category.toLowerCase() === selectedCuisine.toLowerCase();
-        const matchesMethod = selectedMethod === 'All Methods' || item.slaughtered.toLowerCase() === selectedMethod.toLowerCase();
-        const matchesSearch = item.name.toLowerCase().includes(searchQuery.toLowerCase());
-        const matchesLocation = selectedCampusLocation === 'All Locations' || item.campusLocation === selectedCampusLocation;
+        const matchesCategory = selectedCuisine === 'All Cuisines' || item.category?.toLowerCase() === selectedCuisine.toLowerCase();
+        const matchesMethod = selectedMethod === 'All Methods' || item.slaughtered?.toLowerCase() === selectedMethod.toLowerCase();
+        const matchesSearch = item.name?.toLowerCase().includes(searchQuery.toLowerCase());
+        const matchesLocation = selectedCampusLocation === 'All Locations' || String(item.is_on_campus) === selectedCampusLocation;
         return matchesCategory && matchesMethod && matchesSearch && matchesLocation;
       })
       .sort((a, b) => a.name.localeCompare(b.name));
@@ -152,70 +153,71 @@ export default function HalalDirectoryPage() {
             className="border border-gray-300 rounded-lg p-3 sm:p-4 w-full sm:w-56 text-sm sm:text-md focus:ring-2 focus:ring-purple-500 transition"
           >
             <option value="All Locations">All Locations</option>
-            <option value="TRUE">On Campus</option>
-            <option value="FALSE">Off Campus</option>
+            <option value="true">On Campus</option>
+            <option value="false">Off Campus</option>
           </select>
-        </div>
-      </div>
-
-      {/* Main Content Section */}
-      <div className="flex flex-col md:flex-row w-full md:space-x-6">
-        <div className="w-full md:w-2/3">
-          {/* Render filtered restaurant cards */}
-          {filterData().length > 0 ? (
-            filterData().map((item) => (
-              <div key={item.id} className="bg-white mb-6 shadow-lg rounded-lg p-4 sm:p-6 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 hover:shadow-xl transition-shadow duration-300">
-                {/* Image Section */}
-                <div className="w-full sm:w-32 h-32 bg-gray-200 flex items-center justify-center rounded-lg overflow-hidden">
-                  {item.image_id && imageUrls[item.image_id] ? (
-                    <img src={imageUrls[item.image_id]} alt={item.name} className="w-full h-full object-cover" />
-                  ) : (
-                    <p className="text-gray-500">No Image</p>
-                  )}
-                </div>
-
-                {/* Restaurant Details Section */}
-                <div className="flex-grow">
-                  <h2 className="text-lg sm:text-xl font-bold text-primary mb-1">{item.name}</h2>
-                  <p className="text-gray-600 mb-2 text-sm sm:text-md">{item.short_description}</p>
-                  <p className="text-gray-500 text-sm"><strong>Category:</strong> {item.category}</p>
-                  <p className="text-gray-500 text-sm"><strong>Slaughter Method:</strong> {item.slaughtered}</p>
-                  <p className="text-gray-500 text-sm"><strong>Location:</strong> {item.location}</p>
-                  <a href={item.google_maps_link} target="_blank" className="text-blue-600 hover:underline text-sm">View on Google Maps</a>
-                </div>
-
-                {/* Optional Booking Button */}
-                {item.website && (
-                  <a
-                    href={item.website}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-white bg-purple-600 hover:bg-purple-700 transition-colors rounded-lg w-full sm:w-32 h-10 sm:h-12 flex items-center justify-center font-semibold"
-                  >
-                    Book Now
-                  </a>
-                )}
-              </div>
-            ))
-          ) : (
-            <p className="text-gray-600 text-md sm:text-lg">No restaurants found matching your criteria.</p>
-          )}
-        </div>
-
-        {/* Map Section */}
-        <div className="w-full md:w-1/3 sticky top-0 h-96 mt-6 md:mt-0 bg-gray-100 rounded-lg overflow-hidden shadow-lg">
-          <iframe
-            src="https://www.google.com/maps/d/embed?mid=1uQfQqV85aYaziCWMs996FZOPkyIKvAw&usp=sharing"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            allowFullScreen={true}
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
+        
       </div>
     </div>
+
+      {/* Main Content Section */ }
+  <div className="flex flex-col md:flex-row w-full md:space-x-6">
+    <div className="w-full md:w-2/3">
+      {/* Render filtered restaurant cards */}
+      {filterData().length > 0 ? (
+        filterData().map((item) => (
+          <div key={item.id} className="bg-white mb-6 shadow-lg rounded-lg p-4 sm:p-6 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 hover:shadow-xl transition-shadow duration-300">
+            {/* Image Section */}
+            <div className="w-full sm:w-32 h-32 bg-gray-200 flex items-center justify-center rounded-lg overflow-hidden">
+              {item.image_id && imageUrls[item.image_id] ? (
+                <img src={imageUrls[item.image_id]} alt={item.name} className="w-full h-full object-cover" />
+              ) : (
+                <p className="text-gray-500">No Image</p>
+              )}
+            </div>
+
+            {/* Restaurant Details Section */}
+            <div className="flex-grow">
+              <h2 className="text-lg sm:text-xl font-bold text-primary mb-1">{item.name}</h2>
+              <p className="text-gray-600 mb-2 text-sm sm:text-md">{item.short_description}</p>
+              <p className="text-gray-500 text-sm"><strong>Category:</strong> {item.category}</p>
+              <p className="text-gray-500 text-sm"><strong>Slaughter Method:</strong> {item.slaughtered}</p>
+              <p className="text-gray-500 text-sm"><strong>Location:</strong> {item.location}</p>
+              <a href={item.google_maps_link} target="_blank" className="text-blue-600 hover:underline text-sm">View on Google Maps</a>
+            </div>
+
+            {/* Optional Booking Button */}
+            {item.website && (
+              <a
+                href={item.website}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white bg-purple-600 hover:bg-purple-700 transition-colors rounded-lg w-full sm:w-32 h-10 sm:h-12 flex items-center justify-center font-semibold"
+              >
+                Book Now
+              </a>
+            )}
+          </div>
+        ))
+      ) : (
+        <p className="text-gray-600 text-md sm:text-lg">No restaurants found matching your criteria.</p>
+      )}
+    </div>
+
+    {/* Map Section */}
+    <div className="w-full md:w-1/3 sticky top-0 h-96 mt-6 md:mt-0 bg-gray-100 rounded-lg overflow-hidden shadow-lg">
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=1uQfQqV85aYaziCWMs996FZOPkyIKvAw&usp=sharing"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen={true}
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  </div>
+    </div >
   );
 }
 
