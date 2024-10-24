bd4845ebd49f756af66a2f909378ce5f4b04062b
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-10-23 21:27:35 -0400

    db migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241019_184550 from './20241019_184550';
+import * as migration_20241024_012335 from './20241024_012335';
 
 export const migrations = [
   {
     up: migration_20241019_184550.up,
     down: migration_20241019_184550.down,
-    name: '20241019_184550'
+    name: '20241019_184550',
+  },
+  {
+    up: migration_20241024_012335.up,
+    down: migration_20241024_012335.down,
+    name: '20241024_012335'
   },
 ];
