4f80168cbd28d9e49788f212e82b2bcb7665bfe6
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-11-09 16:33:25 -0500

    reverted to normal db-postgres pkg

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241107_215206 from './20241107_215206';
+import * as migration_20241107_232127 from './20241107_232127';
 
 export const migrations = [
   {
-    up: migration_20241107_215206.up,
-    down: migration_20241107_215206.down,
-    name: '20241107_215206'
+    up: migration_20241107_232127.up,
+    down: migration_20241107_232127.down,
+    name: '20241107_232127'
   },
 ];
