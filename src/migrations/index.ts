1409587b0477a25e6da60d408d436f29837509ee
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-11-09 16:41:06 -0500

    merge of main

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241107_232127 from './20241107_232127';
-
-export const migrations = [
-  {
-    up: migration_20241107_232127.up,
-    down: migration_20241107_232127.down,
-    name: '20241107_232127'
-  },
-];
