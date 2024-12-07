985c0774d2f5bd2ea2e8673ab24bc1e868a325f4
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-12-06 23:00:06 -0500

    merge of roommate-service, added formBuildPlugin with overrides, updated packages, payload@3.5, nextjs 15.04 react 19

---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect, useState } from "react";
+import {FaMoon, FaSun} from 'react-icons/fa'
+
+const ThemeToggleButton = () => {
+  const [theme, setTheme] = useState('mytheme'); //Added Theme State
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme') || 'mytheme';
+    setTheme(savedTheme); // Set theme from localStorage or default
+    document.documentElement.setAttribute('data-theme', savedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'mytheme' ? 'mydark' : 'mytheme';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="fixed bottom-4 left-4 p-2 bg-primary text-white rounded-full shadow-lg"
+      aria-label="Toggle Theme"
+    >
+      {theme === 'mytheme' ? <FaMoon /> : <FaSun />}
+    </button>
+  );
+};
+
+export default ThemeToggleButton;
