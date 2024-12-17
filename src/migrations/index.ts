0bf359e810e77b63f3dbc33e37fc669bf59632e4
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-12-16 20:19:56 -0500

    fix twcss err

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241217_011405 from './20241217_011405';
-
-export const migrations = [
-  {
-    up: migration_20241217_011405.up,
-    down: migration_20241217_011405.down,
-    name: '20241217_011405'
-  },
-];
