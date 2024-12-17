0dd98a5f9862f7a6301eb52fc8c709e555097509
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-12-17 13:14:17 -0500

    revert to old middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,9 +1,9 @@
 import { type NextRequest } from 'next/server'
-// import { updateSession } from '@/middleware'
+import { updateSession } from '@/Utils/middleware'
 
-// export async function middleware(request: NextRequest) {
-//   return await updateSession(request)
-// }
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
 
 
 // Optionally, don't invoke Middleware on some paths
