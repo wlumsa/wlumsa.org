c01ca4acea01516045c1ee9f0f6f83c5f205de0b
rzlm <razan4424@gmail.com>
2025-02-25 14:45:02 -0500

    remove text-center

--- src/components/Forms/FormBlock/Form/Message/index.tsx
+++ src/components/Forms/FormBlock/Form/Message/index.tsx
@@ -8,7 +8,7 @@ import { Width } from '../Width'
 export const Message: React.FC<MessageField> = ({ message }) => {
   return (
     <Width width={100}>
-      <div className="min-h-[18rem]  text-center justify-center text-2xl my-4">
+      <div className="min-h-[18rem]  md:text-center justify-center text-2xl my-4">
       <RichText className=" my-2 text-gray-700 " content={message} />
       </div>  
     </Width>
