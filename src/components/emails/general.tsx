88166727d6e49f6601bd1e71789542170297c32f
rzlm <razan4424@gmail.com>
2025-09-09 11:59:02 -0400

    changed msa guidebook link

--- src/components/emails/general.tsx
+++ src/components/emails/general.tsx
@@ -49,7 +49,7 @@ export const Newsletter = ({ firstName, content }: WelcomeEmailProps) => (
 
       <Container style={container}>
         <Img
-          src={"https://qxhgmdhdnavuvrexvjhw.supabase.co/storage/v1/object/public/wlumsa_storage_bucket_test/MSA%20Logo-2.png"} // url || ""
+          src={"https://wlumsa.org/msa_guidebook.pdf"} // url || ""
           width="50"
           height="50"
           alt="logo"
