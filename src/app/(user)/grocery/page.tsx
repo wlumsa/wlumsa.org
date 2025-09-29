cff7f40084512e30c6629bb388fae6c611c1a6c0
rzlm <uwtaqwacon@gmail.com>
2025-09-28 21:50:09 -0400

    grocery page:

--- src/app/(user)/grocery/page.tsx
+++ src/app/(user)/grocery/page.tsx
@@ -1,10 +1,11 @@
 // import GroceryStoresClient from "./groceryStoresClient";
 // import { getHalalGroceryStores } from "@/Utils/datafetcher";
 
-// // Main component for the Halal Grocery Stores page
-// export default async function GroceryStoresPage() {
-//   // Fetch all grocery stores
+// Main component for the Halal Grocery Stores page
+export default async function GroceryStoresPage() {
+  // Fetch all grocery stores
 //   const groceryStores = await getHalalGroceryStores();
-
+return <>
+</>
 //   return <GroceryStoresClient groceryStores={groceryStores} />;
-// }
+}
