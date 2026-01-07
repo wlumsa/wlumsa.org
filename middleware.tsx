ea786948e73353900317e71285c4394de2bc2552
rzlm <razan4424@gmail.com>
2026-01-06 20:55:46 -0500

    fixed error

--- middleware.tsx
+++ middleware.tsx
@@ -1,5 +1,5 @@
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/proxy'
+import { updateSession } from '@/middleware'
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
