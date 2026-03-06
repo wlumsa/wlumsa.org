c698b5b67244af3e4dc5ff1aad83eae6f5bba9f5
Maruf <hossain.maruf186@gmail.com>
2026-03-06 09:10:22 -0500

    Add migration for weekly_events locationLink

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260227_015008 from './20260227_015008';
+import * as migration_20260306_141008 from './20260306_141008';
 
 export const migrations = [
   {
     up: migration_20260227_015008.up,
     down: migration_20260227_015008.down,
     name: '20260227_015008',
+  },
+  {
+    up: migration_20260306_141008.up,
+    down: migration_20260306_141008.down,
+    name: '20260306_141008'
   },
 ];
